602043244ab7c3b03df7fdd86fecdee1069b8594
Levi <levi.durfee@gmail.com>
2016-12-08 23:17:35 -0500

    Adding a function and an 'object'

--- main.go
+++ main.go
@@ -1,8 +1,46 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main() {
-	fmt.Printf("Hello\n")
+type Person struct {
+	Id int
+	Name string
+	Age int
+	Pet Pet
 }
 
+type Pet struct {
+	PetName string
+	PetType string
+	PetAge int
+}
+
+func (p *Person) getPet() {
+	dogAge := strconv.Itoa(p.Pet.PetAge)
+	dogYears := strconv.Itoa(p.Pet.PetAge * 7)
+	fmt.Printf("Hi, I am " + p.Name + " and I have a pet named " + p.Pet.PetName + ", he is " + dogAge + " in human years. That makes him " + dogYears + " in dog years!\n")
+}
+
+func gone(s string) {
+	p := Person {
+		Id: 1,
+		Name: "Levi",
+		Age: 32,
+		Pet: Pet {
+			PetName: "Corky",
+			PetType: "Mixed",
+			PetAge: 9,
+		},
+	}
+
+	p.getPet()
+
+	fmt.Printf(s + "\n")
+}
+
+func main() {
+	gone("bye!")
+}
