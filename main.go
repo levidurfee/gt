08119fef0d38343897f83a44c643759a1270a5f1
Levi <levi.durfee@gmail.com>
2016-12-10 10:59:08 -0500

    Changing GivePets to return a string

--- main.go
+++ main.go
@@ -2,35 +2,36 @@ package main
 
 import (
 	"fmt"
-	"strconv"
 	"github.com/levidurfee/stringutil"
+	"strconv"
 )
 
 type Person struct {
-	Id int
+	Id   int
 	Name string
-	Age int
-	Pet Pet
+	Age  int
+	Pet  Pet
 }
 
 type Pet struct {
 	PetName string
 	PetType string
-	PetAge int
+	PetAge  int
 }
 
 type Petter interface {
 	GivePets(p Pet) string
 }
 
-func (p *Person) GivePets(pet Pet) {
-	fmt.Printf(p.Name + " gives " + pet.PetName + " 100 pets.. aww.. good pet!\n")
+func (p *Person) GivePets(pet Pet) string {
+	return p.Name + " gives " + pet.PetName + " 100 pets.. aww.. good pet!"
 }
 
 func (p *Person) getPet() {
 	dogAge := strconv.Itoa(p.Pet.PetAge)
 	dogYears := strconv.Itoa(p.Pet.PetAge * 7)
-	fmt.Printf("Hi, I am " + p.Name + " and I have a pet named " + p.Pet.PetName + ", he is " + dogAge + " in human years. That makes him " + dogYears + " in dog years!\n")
+	fmt.Printf("Hi, I am " + p.Name + " and I have a pet named " + p.Pet.PetName + ", he is " + dogAge +
+		" in human years. That makes him " + dogYears + " in dog years!\n")
 }
 
 func gone(s string) {
@@ -38,19 +39,19 @@ func gone(s string) {
 }
 
 func main() {
-	p := Person {
-		Id: 1,
+	p := Person{
+		Id:   1,
 		Name: "Levi",
-		Age: 32,
-		Pet: Pet {
+		Age:  32,
+		Pet: Pet{
 			PetName: "Corky",
 			PetType: "Mixed",
-			PetAge: 9,
+			PetAge:  9,
 		},
 	}
 
 	p.getPet()
-	p.GivePets(p.Pet)
+	fmt.Printf(p.GivePets(p.Pet))
 
 	gone("bye!")
 	fmt.Printf(stringutil.Reverse("Oh helloo!!!"))
