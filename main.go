22f5e6f4cf5bbea282f76f7800181c51b9455e1c
Levi <levi.durfee@gmail.com>
2016-12-11 04:37:53 -0500

    Adding another route

--- main.go
+++ main.go
@@ -9,7 +9,12 @@ func hello(w http.ResponseWriter, r *http.Request) {
 	io.WriteString(w, "Hello world!")
 }
 
+func levi(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, Levi.")
+}
+
 func main() {
 	http.HandleFunc("/", hello)
+	http.HandleFunc("/levi", levi)
 	http.ListenAndServe(":8000", nil)
 }
